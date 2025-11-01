4c4bd3a716da128955d3c297fcf95a1eca492147
Moisés Marques Pereira <mpm10433@gmail.com>
2025-10-31 22:39:20 -0300

    Corrigido index.tsx duplicado e redirecionamento do login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,16 @@
+
+
 // frontend/src/pages/index.tsx
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { clearToken } from '../utils/auth' // 👈 usa a função que já existe
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false,
+    },
+  };
+}
 
 export default function Home() {
-  const router = useRouter()
-
-  useEffect(() => {
-    // 🔥 limpa o token toda vez que abrir o site
-    clearToken()
-
-    // vai direto pro login
-    router.replace('/login')
-  }, [router])
-
-  return null
+  return null;
 }
+
